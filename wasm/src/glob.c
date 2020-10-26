9290a82beed3345b3dab91576453a62e2db7e734
xescugc <xescugil@gmail.com>
2020-10-26 14:03:35 +0100

    fixup! wasm/src/glob: Implement the 'glob.Match' function in C

--- wasm/src/glob.c
+++ wasm/src/glob.c
@@ -5,9 +5,9 @@
 
 opa_value *opa_glob_match(opa_value *p, opa_value *d, opa_value *m)
 {
-	opa_string_t *ps = opa_cast_string(p);
-	opa_string_t *ds = opa_cast_string(d);
-	opa_string_t *ms = opa_cast_string(m);
+  opa_string_t *ps = opa_cast_string(p);
+  opa_string_t *ds = opa_cast_string(d);
+  opa_string_t *ms = opa_cast_string(m);
 
   int r = fnmatch(ps->v, ms->v, 0);
 
