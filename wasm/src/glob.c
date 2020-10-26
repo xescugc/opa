7ea5633ad414be5bf5180360863b1f42bef709a7
xescugc <xescugil@gmail.com>
2020-10-26 14:03:26 +0100

    wasm/src/glob: Implement the 'glob.Match' function in C
    It uses the fnmatch to match the pattern with the value

---
+++ wasm/src/glob.c
@@ -0,0 +1,19 @@
+#include "glob.h"
+#include "stdio.h"
+#include "fnmatch.h"
+
+
+opa_value *opa_glob_match(opa_value *p, opa_value *d, opa_value *m)
+{
+	opa_string_t *ps = opa_cast_string(p);
+	opa_string_t *ds = opa_cast_string(d);
+	opa_string_t *ms = opa_cast_string(m);
+
+  int r = fnmatch(ps->v, ms->v, 0);
+
+  if (r == 0)
+  {
+    return opa_boolean(true);
+  }
+  return opa_boolean(false);
+}
---
+++ wasm/src/glob.h
@@ -0,0 +1,3 @@
+#include "value.h"
+
+opa_value *opa_glob_match(opa_value *p, opa_value *d, opa_value *m);
